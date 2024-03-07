6389baacda880c5995eb4593c9f3d830dd8c2ab9
Dante <vettori.dante@itismeucci.com>
2024-03-07 18:48:21 +0100

    work here

---
+++ scriptLeaflet.js
@@ -0,0 +1,50 @@
+const LatI = document.querySelector("#LatI");
+const LngI = document.querySelector("#LngI");
+const btn = document.querySelector("#submit");
+const mark = document.querySelector("#isMarker");
+let arcipressi = [43.7674522 , 11.2095389];
+let map = L.map('map')//L chiamata a LeafLet poi Bo
+map.setView([43.7659151, 11.2135399], 15);//tra [latitudine , longitudine] , valore zoom
+/*Zoom: valore 0 = pianeta intero , 19 valore massimo di zoom(consigliato) */
+
+//aggiunta del layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//aggiungo il marker
+let marker = L.marker([43.7659151, 11.2135399]).addTo(map);
+let marker2 = L.marker(arcipressi).addTo(map);
+let marker3 = L.marker([43.7656236 , 11.2067503]).addTo(map);
+
+let circle = L.circle(arcipressi, {color: "yellow", radius: 400}).addTo(map);
+marker._icon.classList.add("red"); //aggiunge classi al marker
+marker3._icon.classList.add("green"); //aggiunge classi al marker
+//ono contiene filter:hue-rotate(num deg);
+//filter: hue-rotate = letia la tonailtà del colore
+marker.bindPopup("<b>Siamo Qui</b><br>4A-IA");
+// interagiendo con il marker.bindPopup(stringa).openPopup
+//Sintassi Si riferisce da L.metodo.addTo(oggetto)
+
+let link = []; // creo una array contenitore
+
+//aggiungo laitudine e longitudine dei punti da collegare
+link.push(marker.getLatLng());
+link.push(marker2.getLatLng());
+link.push(marker3.getLatLng());
+
+//creo i collegamenti tramite polyline();
+let line = L.polyline(link, {color: 'black'}).addTo(map);
+
+
+function goTo(){
+    let tmpA = [LatI.value , LngI.value];
+    console.log(mark.checked);
+    map.setView(tmpA, 13);
+    if(mark.checked){
+        let markT = L.marker(tmpA).addTo(map);
+    }
+}
+
+btn.addEventListener("click" , goTo);
