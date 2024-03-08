1b830e1d42dd7bb0532c04891a043fe4c6782a26
unknown <vettori.dante@itismeucci.com>
2024-03-08 17:17:54 +0100

    Molteplici mappe aggiunte/navbar

--- scriptLeaflet.js
+++ scriptLeaflet.js
@@ -3,16 +3,35 @@ const LatI = document.querySelector("#LatI");//latitudine form
 const LngI = document.querySelector("#LngI");//longitudine form
 const btn = document.querySelector("#submit");//btn form
 const mark = document.querySelector("#isMarker");//verifica sull'inserimento del marker in goTo
+const rmBtn = document.querySelector("#remove");
 let arcipressi = [43.7674522 , 11.2095389];//salvo le coordinate di una posizione
+let addedMark = [];
 let map = L.map('map')//L chiamata a LeafLet creando la mapp
+let map2 = L.map('map2');
+let map3 = L.map('map3');
+let map4 = L.map('map4');
+
 map.setView([43.7659151, 11.2135399], 15);//tra [latitudine , longitudine] , valore zoom
 /*Zoom: valore 0 = pianeta intero , 19 valore massimo di zoom(consigliato) */
+map2.setView(arcipressi, 17);
+map3.setView(arcipressi, 16);
+map4.setView(arcipressi, 15);
+
 
 //aggiunta del layer
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function addDefaultLayer(mappa){
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    }).addTo(mappa);
+}
+
+
+let m1 = L.marker(arcipressi).addTo(map2);//Uguale a sopra ma non si deve mai usare
+//lo stesso marker in 2 mappe (Funziona solo in una e male)
+m1.bindPopup("Un semplice marker");
+m1.openPopup();
+
 
 //aggiungo il marker (usati nella prima mappa)
 let marker = L.marker([43.7659151, 11.2135399]).addTo(map);
@@ -45,9 +64,27 @@ function goTo(){
     map.setView(tmpA, 13);
     if(mark.checked){
         let markT = L.marker(tmpA).addTo(map);
+        addedMark.push(markT);
+    }
+}
+
+function removeMarkers(){
+    for(let i of addedMark){
+        map.removeLayer(i);
     }
 }
 
 
 //Events
+addEventListener("DOMContentLoaded" , () => {
+    addDefaultLayer(map);
+    addDefaultLayer(map2);
+    addDefaultLayer(map3);
+    addDefaultLayer(map4);
+    L.polyline(link, {color: "black"}).addTo(map3);
+    L.circle(arcipressi, {color: "yellow", radius: 400}).addTo(map4)
+})
 btn.addEventListener("click" , goTo);
+rmBtn.addEventListener("click", removeMarkers);
+
+//per rimuovere è possibile usare map.removeLayer(oggetto)
